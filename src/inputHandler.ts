cbd23f3c51bb165570d045b4f1f4b1d21bbdc18a
angela~ <53804450+angelawangits@users.noreply.github.com>
2024-01-17 21:39:35 +0800

    feat: finish inputHandler.ts

--- src/inputHandler.ts
+++ src/inputHandler.ts
@@ -11,6 +11,9 @@
  * 輸出: 'Input is a number: 123'
  */
 
-export function handleInput(input) {
-    // 在此實現函式
+export function handleInput(input: string | number): string {
+    if (typeof input == "string") {
+        return `Input is a string: ${input}`;
+    }
+    return `Input is a number: ${input}`;
 }
