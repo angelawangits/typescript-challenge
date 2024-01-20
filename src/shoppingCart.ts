b2d220359432f85e4ed636cc4994d1eacfa66e37
angela~ <53804450+angelawangits@users.noreply.github.com>
2024-01-20 14:11:33 +0800

    feat: finish shoppingCart.ts

--- src/shoppingCart.ts
+++ src/shoppingCart.ts
@@ -1,3 +1,5 @@
+import {it} from "vitest";
+
 interface Product {
     id: string;
     name: string;
@@ -20,8 +22,12 @@ export function createShoppingCart() {
      * @returns - 回傳購物車中所有商品的總價
      * 範例：getTotalPrice() 應該回傳 300，假設購物車中有兩個商品，價格分別為 100 和 200
      */
-    function getTotalPrice() {
-        // 請在此處寫下你的程式碼
+    function getTotalPrice(): number {
+        let totalPrice: number = 0;
+        items.forEach(item => {
+            totalPrice += item.price
+        });
+        return totalPrice;
     }
 
     function getItemCount() {
