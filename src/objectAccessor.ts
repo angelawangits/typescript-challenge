d366d67c3f355f934378e0cfb20df5f15fb311ce
angela~ <53804450+angelawangits@users.noreply.github.com>
2024-01-20 13:58:26 +0800

    feat: finish obbjectAccessor.ts

--- src/objectAccessor.ts
+++ src/objectAccessor.ts
@@ -13,5 +13,12 @@
  * @returns - 回傳一個物件，該物件有 get 和 set 兩個方法
  */
 export function createObjectAccessor<T>(obj: T) {
-    // 請在此處寫下你的程式碼
+    return {
+        get<K extends keyof T>(key: K): T[K] {
+            return obj[key]
+        },
+        set<K extends keyof T>(key: K, value: T[K]): void {
+            obj[key] = value;
+        }
+    }
 }
